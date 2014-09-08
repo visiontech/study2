db27c082b27a58325c1f461fe2f7a8c42190d3b1
Syma <Syma@192.168.1.101>
2014-09-08 20:27:58 +0300

    готово

--- src/ua/viasat/www/Homework1.java
+++ src/ua/viasat/www/Homework1.java
@@ -1,61 +1,76 @@
 package ua.viasat.www;
 
 public class Homework1 {
- public static	double sum = 100;
- public static	double discount = 0;
- 
+	public static double sum = 100;
+	public static double discount = 0;
 
 	private static void getSum(Double sum) {
-		 
-	String id = String.valueOf(sum/100).substring(0,1);
-	
-	switch(id){
-	case "0": discount=0;
-	break;
-	case "1": discount=0;
-	break;
-	case "2": discount=0;
-	break;
-	case "3": discount=3;
-	break;
-	case "4": discount=3;
-	break;
-	case "5": discount=5;
-	break;
-	case "6": discount=5;
-	break;
-	case "7": discount=5;
-	break;
-	case "-": discount=0; 
-	sum=0.00; 
-	System.out.println("ОШИБКА!!! Сумма оплаты меньше нуля");
-	break;
-	default:discount=10;
-	break;
-	}
-	
-		System.out.println("Итого к оплате: "+sum*(1-discount/100)+" Ваша скидка: "+discount+"%");
-}	
+
+		String id = String.valueOf((int) (sum / 100));
+
+		int mod = (int) (Math.abs((sum / 100)) / (sum / 100)); //Расчет знака суммы
 		
-	private static void getSumIf(Double sum) {
-				
-		if(sum<300) {		
+
+		switch (mod) {
+		case -1:
 			discount = 0;
-		}else if(sum<500){
+			sum = 0.00;
+			System.out.println("ОШИБКА!!! Сумма должна быть >=0");
+		break;
+		default:
+			switch (id) {
+			case "0":
+				discount = 0;
+				break;
+			case "1":
+				discount = 0;
+				break;
+			case "2":
+				discount = 0;
+				break;
+			case "3":
+				discount = 3;
+				break;
+			case "4":
+				discount = 3;
+				break;
+			case "5":
+				discount = 5;
+				break;
+			case "6":
+				discount = 5;
+				break;
+			case "7":
+				discount = 5;
+				break;
+			default:
+				discount = 10;
+				break;
+			}
+		}
+		System.out.println("Итого к оплате: " + sum * (1 - discount / 100)
+				+ " Ваша скидка: " + discount + "%");
+	}
+
+	private static void getSumIf(Double sum) {
+
+		if (sum < 300) {
+			discount = 0;
+		} else if (sum < 500) {
 			discount = 3;
-		}else if(sum<800) {
-			discount=5;
-		}else discount = 10;
-		double result = sum * (1-discount/100);
-		System.out.println("Итого к оплате: "+ result + " Ваша скидка: " +discount+"%");
-	}	
-	
+		} else if (sum < 800) {
+			discount = 5;
+		} else
+			discount = 10;
+		double result = sum * (1 - discount / 100);
+		System.out.println("Итого к оплате: " + result + " Ваша скидка: "
+				+ discount + "%");
+	}
+
 	public static void main(String[] args) {
-	
-		//getSumIf(799.00);
-		getSum(-800.00);
-	}	
-	
+
+		// getSumIf(799.00);
+		getSum(-4000.00);
+	}
+
 }
-
-
