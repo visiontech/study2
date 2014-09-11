4e08a98ea78a401cfee455a1227efe73c33968c7
nshkarupa <nshkarupa@Shkarupa.vtv.local>
2014-09-11 10:28:03 +0300

    +5

--- src/ua/viasat/www/Homework2.java
+++ src/ua/viasat/www/Homework2.java
@@ -1,11 +1,12 @@
 package ua.viasat.www;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Homework2 {
 
-	public static void task1() { // 1.отсортировать массив пузырьковым методом
-		// вариант
+	public static void task1_1() { // 1.отсортировать массив пузырьковым методом
+		// вариант 1
 		int temp;
 		int a[] = { 9, 5, 7, 6, 0, 3, 1, 2, 4, 8 };
 		for (int k = 0; k < a.length - 1; k++) {
@@ -20,6 +21,14 @@ public class Homework2 {
 		System.out.println(Arrays.toString(a));
 	}
 
+	public static void task1_2() { // 1.отсортировать массив пузырьковым методом
+		// вариант 2
+		int temp;
+		int a[] = { 9, 5, 7, 6, 0, 3, 1, 2, 4, 8 };
+		Arrays.sort(a);
+		System.out.println(Arrays.toString(a));
+	}
+
 	public static void task2(int year) {
 		/*
 		 * 2. Создать массив из 12 элементов, каждый элемент является
@@ -27,9 +36,12 @@ public class Homework2 {
 		 * количеством строк.
 		 */
 
-		int a[] = new int[12]; 	int k = 0; 	
+		int a[] = new int[12];
+		int k = 0;
 		for (int i = 0; i < a.length; i++) {
-			a[i] = 31 - k; 	k = i < 6 ? (i % 2 != 0 ? 0 : 1) : (i % 2 == 0 ? 0 : 1); a[1] = year % 4 == 0 ? 29 : 28;
+			a[i] = 31 - k;
+			k = i < 6 ? (i % 2 != 0 ? 0 : 1) : (i % 2 == 0 ? 0 : 1);
+			a[1] = year % 4 == 0 ? 29 : 28;
 		}
 
 		System.out.println(Arrays.toString(a));
@@ -37,7 +49,7 @@ public class Homework2 {
 
 	public static void task3_1(int n) {
 		/*
-		 * 2. В цикле вывести 21 число фибоначчи вариант1
+		 * 3. В цикле вывести 21 число фибоначчи вариант1
 		 */
 		int feb[] = new int[3];
 		feb[0] = 0;
@@ -52,19 +64,48 @@ public class Homework2 {
 		}
 	}
 
+	private static int[] swapper(int m[], int a, int b) {// метод перемещения  а и в
+		int temp;
+		temp = m[a];
+		m[a] = m[b];
+		m[b] = temp;
+		return m;
+	}
+
 	public static void task4() {
-		//4. Сделать реверс масива 
-		
-		
+		// 4. Сделать реверс масива
+		int temp;
+		int a[] = { 9, 5, 7, 6, 0, 3, 1, 2, 4, 8, 0 };
+		for (int i = 0; i < a.length / 2; i++) {
+			a = swapper(a, i, a.length - 1 - i);
+
+		}
+		System.out.println(Arrays.toString(a));
+	}
+	
+	public static void task5() {
+		// 4. вводится латинская буква - гласная она или согласная
+		System.out.println("Enter the Latin letter:>");
+		Scanner reader = new Scanner(System.in);
+		if(reader.hasNext("\\D")&&reader.next("\\D").length()==1){
+			String n = reader.next("\\D");
+		} else System.out.println("Incorrect enter!!! You can not enter number and null!!");
+		 
 	}
 
 	public static void main(String[] args) {
-		System.out.println("__________________________ЗАДАНИЕ 1_____________________________");
-		task1();
+		System.out.println("__________________________ЗАДАНИЕ 1 (Вариант 1)_________________________");
+		task1_1();
+		System.out.println("__________________________ЗАДАНИЕ 1 (Вариант 2)__________________________");
+		task1_2();
 		System.out.println("__________________________ЗАДАНИЕ 2_____________________________");
 		task2(2014);
 		System.out.println("__________________________ЗАДАНИЕ 3_____________________________");
 		task3_1(21);
+		System.out.println("__________________________ЗАДАНИЕ 4_____________________________");
+		task4();
+		System.out.println("__________________________ЗАДАНИЕ 5_____________________________");
+		task5();
 	}
 
 }
